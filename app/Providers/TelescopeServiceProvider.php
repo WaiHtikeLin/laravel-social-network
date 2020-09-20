269f9dbbe30af4b98633d46947f2a7363d738fc3
waihtikelin <waihtikelin279@gmail.com>
2020-09-20 15:06:46 +0630

    made telescope listen on production

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -20,17 +20,17 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 
         $this->hideSensitiveRequestDetails();
 
-        Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
-                return true;
-            }
-
-            return $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
-        });
+        // Telescope::filter(function (IncomingEntry $entry) {
+        //     if ($this->app->environment('local')) {
+        //         return true;
+        //     }
+        //
+        //     return $entry->isReportableException() ||
+        //            $entry->isFailedRequest() ||
+        //            $entry->isFailedJob() ||
+        //            $entry->isScheduledTask() ||
+        //            $entry->hasMonitoredTag();
+        // });
     }
 
     /**
