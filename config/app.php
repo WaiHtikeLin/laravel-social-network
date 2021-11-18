63387bde44334d7abcf7603dff7e5be828cf5a7a
waihtikelin <waihtikelin279@gmail.com>
2021-11-18 21:53:37 +0630

    updated

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://connect-online.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
