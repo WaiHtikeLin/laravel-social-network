142a0a6fb348a5feeaab4fce7fff3b68acd3ae95
waihtikelin <waihtikelin279@gmail.com>
2021-11-19 15:59:12 +0630

    updated

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+      if (!$request->secure()) {
+             return redirect()->secure($request->getRequestUri());
+         }
+
+        return $next($request);
+    }
+}
